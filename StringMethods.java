ddb45f665447ecd3653f50ee508f808c128e8644
TanushkaVadapalli <2311CS010666@mallareddyuniversity.ac.in>
2025-12-24 10:13:14 +0530

    Day4 String Methods in java

---
+++ StringMethods.java
@@ -0,0 +1,22 @@
+public class StringMethods {
+    // public static void main(String[] args) {
+    // String s = "Computer Science Engineer";
+    // s = s.concat(" Delta");
+    // System.out.println(s);
+    // }
+
+    public static void main(String[] args) {
+        String s = "Computer Science Engineer";
+        String r = "I am";
+        s = s.concat(" Delta ");
+        r = r.concat(" Tanushka");
+        System.out.println(s);
+        System.out.println(r);
+
+        String a = "CSE";
+        System.out.println(a.length());
+        System.out.println(a.charAt(0));
+        System.out.println(a.charAt(2));
+
+    }
+}
